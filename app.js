18831af6289e353cf3fcd7fc5e8f6e94e4f4bbe4
Sameehqatabi <sameeh.alqatabi@snhu.edu>
2023-02-13 00:39:01 -0500

    RESTful API

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var hbs = require('hbs');
 require("./app_api/database/db");
 
+
+
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
@@ -14,6 +16,7 @@ var newsRouter = require("./app_server/routes/news");
 var mealsRouter = require("./app_server/routes/meals");
 var contactRouter = require("./app_server/routes/contact");
 var aboutRouter = require("./app_server/routes/about");
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -40,6 +43,7 @@ app.use("/news", newsRouter);
 app.use("/meals", mealsRouter);
 app.use("/contact", contactRouter);
 app.use("/about", aboutRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
